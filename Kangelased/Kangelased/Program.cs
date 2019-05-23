21180982479ae433f3a96360cbd74eab7e1979ab
opilane <opilane@HP-CZC6297BBG>
2019-05-23 18:04:46 +0300

    Loo LaeKangelased meetod ja peaklassis vajalikud muutujad

--- Kangelased/Kangelased/Program.cs
+++ Kangelased/Kangelased/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Kangelased
 {
     class Program
     {
+        static List<Kangelane> kangelased = new List<Kangelane>();
+
+        static void LaeKangelased(string failiNimi)
+        {
+            string[] failiandmed = File.ReadAllLines(failiNimi);
+            foreach (var item in failiandmed)
+            {
+                string[] nimi_ja_koht = item.Split('/');
+                for (int i = 0; i < nimi_ja_koht.Length; i++)
+                {
+                    nimi_ja_koht[i] = nimi_ja_koht[i].Trim();
+                }
+                if (nimi_ja_koht[0].EndsWith("*"))
+                { 
+                    kangelased.Add(new SuperKangelane(nimi_ja_koht[0], nimi_ja_koht[1])); 
+                }
+                else
+                {
+                    kangelased.Add(new Kangelane(nimi_ja_koht[0], nimi_ja_koht[1]));
+                }
+            }
+        }
         static void Main(string[] args)
         {
+
         }
     }
 }
