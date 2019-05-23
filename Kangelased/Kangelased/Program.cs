bbd2e41fd6b7723583587cee5802b26000cf74d0
opilane <opilane@HP-CZC6297BBG>
2019-05-23 18:36:11 +0300

    Lisa Tere

--- Kangelased/Kangelased/Program.cs
+++ Kangelased/Kangelased/Program.cs
@@ -10,7 +10,7 @@ namespace Kangelased
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Tere!");
         }
     }
 }
