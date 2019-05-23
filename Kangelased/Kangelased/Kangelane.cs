71d7f7263d7a8981628356a888b99f27a7a22468
opilane <opilane@HP-CZC6297BBG>
2019-05-23 16:38:21 +0300

    Loo kangelase klass ja kirjuta nõutud meetodid

---
+++ Kangelased/Kangelased/Kangelane.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kangelased
+{
+    class Kangelane
+    {
+        private string _Nimi;
+        private string _Asukoht;
+        public string Nimi { get => _Nimi; set => _Nimi = value; }
+        public string Asukoht { get => _Asukoht; set => _Asukoht = value; }
+
+        public Kangelane(string kangelaseNimi, string kangelaseAsukoht)
+        {
+            Nimi = kangelaseNimi;
+            Asukoht = kangelaseAsukoht;
+        }
+
+        public int Päästa(int inimesteArv)
+        {
+            double a = Math.Round((inimesteArv * 0.95),0);
+            return (int)a;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nimi} kaitseb {Asukoht}";
+        }
+    }
+}
